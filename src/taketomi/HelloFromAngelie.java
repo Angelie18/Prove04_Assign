5392011e0f96b02a5f45c82cc72363b8a0bf6244
Angelie <angelietaketomi@gmail.com>
2022-01-27 14:38:40 +0900

    Initial commit

---
+++ src/taketomi/HelloFromAngelie.java
@@ -0,0 +1,13 @@
+package taketomi;
+
+public class HelloFromAngelie {
+        public void selfIntro() {
+            System.out.println("Hello, my name is Angelie Taketomi.");
+        }
+
+    public static void main(String[] args) {
+	// write your code here
+        HelloFromAngelie hello = new HelloFromAngelie();
+        hello.selfIntro();
+    }
+}
