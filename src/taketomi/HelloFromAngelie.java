2c635ed902feef3b210a738be29fecdbf6f7c530
Matthew Home <matthew@vogcalgaryappdeveloper.com>
2022-01-27 23:20:55 -0700

    added guestIntro() function called guestIntro()

--- src/taketomi/HelloFromAngelie.java
+++ src/taketomi/HelloFromAngelie.java
@@ -1,13 +1,19 @@
 package taketomi;
 
 public class HelloFromAngelie {
-        public void selfIntro() {
-            System.out.println("Hello, my name is Angelie Taketomi.");
-        }
+
+    public void selfIntro() {
+        System.out.println("Hello, my name is Angelie Taketomi.");
+    }
+
+    public void guestIntro(String guestName) {
+        System.out.printf("Hello Angelie, my name is %s.%n", guestName);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         HelloFromAngelie hello = new HelloFromAngelie();
         hello.selfIntro();
+        hello.guestIntro("Matthew Bates");
     }
 }
